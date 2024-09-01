b3d10a23932917c962ff1837f3905eb6137f5ac4
NexGenCoder756 <nexgencoder6@gmail.com>
2024-09-01 20:28:26 -0300

    Test software report 2024-09-01 20:28:30

---
+++ Create_Testing_in_Software_Development/test_python_software.py
@@ -0,0 +1,6 @@
+{
+    "test_file": "Create_Testing_in_Software_Development/test_python_software.py",
+    "timestamp": "2024-09-01T20:28:28.993517",
+    "status": "success",
+    "details": "============================= test session starts =============================\nplatform win32 -- Python 3.9.1, pytest-8.3.2, pluggy-1.5.0\nrootdir: C:\\Users\\ualer\\Downloads\\Saas do site\\A-I-O-R-G\nplugins: anyio-4.3.0, dash-2.14.2, Faker-22.4.0, json-report-1.5.0, metadata-3.1.1, typeguard-4.2.1\ncollected 2 items\n\nCreate_Testing_in_Software_Development\\test_python_software.py ..        [100%]\n\n============================== 2 passed in 6.08s ==============================\n"
+}
