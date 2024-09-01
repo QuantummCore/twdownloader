a8ba6e56fe27c0eefe9a5cac0062adfd20eeced2
NexGenCoder756 <nexgencoder6@gmail.com>
2024-09-01 16:34:47 -0300

    Test software report 2024-09-01 16:34:50

---
+++ Create_Testing_in_Software_Development/python_software.py
@@ -0,0 +1,6 @@
+{
+    "test_file": "Create_Testing_in_Software_Development/python_software.py",
+    "timestamp": "2024-09-01T16:34:49.514494",
+    "status": "failure",
+    "details": "\nno tests ran in 0.59s\n"
+}
